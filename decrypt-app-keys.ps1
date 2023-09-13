0a0071d493abbe42acdd0f321dfeeb151ffbe223
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-13 11:19:15 +0530

    Update decrypt-app-keys.ps1

--- decrypt-app-keys.ps1
+++ decrypt-app-keys.ps1
@@ -79,10 +79,10 @@ $targetFilePath = "decrypt/decrypt-appkeys.json"  # Replace with the actual file
 $githubToken = $git_token
 
 # Encode the decrypted content as base64
-$base64Content = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($decrypteddata)
+$base64Content = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($decrypteddata))
 
 # Define the API URL to create/update the file in the target branch
-$apiUrl = "https://api.github.com/repos/$githubUsername/$repositoryName/contents/$targetFilePath"
+$apiUrl = "https://api.github.com/repos/"+$githubUsername+"/"+$repositoryName+"/contents/"+$targetFilePath
 
 # Create a JSON body for the API request
 $requestBody = @{
