a18c434727fcabce6160eb71f9b66b9fa2b14b85
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-14 00:26:07 +0530

    Update decrypt-json-payload.ps1

--- decrypt-json-payload.ps1
+++ decrypt-json-payload.ps1
@@ -2,6 +2,13 @@ $git_token = $env:token
 
 $fileContent = $env:jsonInput
 
+Write-Host "fileContent: $fileContent"
+
+# Specify the fields you want to encrypt
+$fieldsToDecrypt = $env:fieldsToEncrypt -split ","
+
+Write-Host "fieldsToDecrypt: $fieldsToDecrypt"
+
 try {
     # Parse and display the file content (in this case, it's assumed to be JSON)
     $jsonContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($fileContent.content))
