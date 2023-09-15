cd4659efdab56eb8cb1e2799d9a073b8e28533d2
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-15 17:50:50 +0530

    Update encrypt-kvmdata.ps1

--- encrypt-kvmdata.ps1
+++ encrypt-kvmdata.ps1
@@ -32,6 +32,7 @@ try {
 
     # Load JSON content from the file
     $jsonContent = Get-Content $env:JSON_FILE_PATH -Raw | ConvertFrom-Json
+    Write-Host "jsonContent: $jsonContent"
 
     # Decryption key
     $keyHex = $env:key
@@ -51,6 +52,7 @@ try {
 
     # Loop through the JSON data and encrypt specified fields
     foreach ($entry in $jsonContent.$fieldPath) {
+        Write-Host "Entered into FOREACH...!"
         # Call the Encrypt-Fields function to encrypt the specified fields
         $entry = Encrypt-Fields -data $entry -fieldsToEncrypt $fieldsToEncrypt -AES $AES
     }
@@ -60,71 +62,71 @@ try {
 
     Write-Host "Encrypted data: $encryptedJsonData"
 
-    # Define your GitHub username, repository names, branch names, and file paths
-    $githubUsername =  $env:targetgithubUsername
-    $repositoryName = $env:targetrepositoryName
-    $targetBranchName = $env:targetBranchName
-    $targetFilePath = $env:targetFilePath
+    # # Define your GitHub username, repository names, branch names, and file paths
+    # $githubUsername =  $env:targetgithubUsername
+    # $repositoryName = $env:targetrepositoryName
+    # $targetBranchName = $env:targetBranchName
+    # $targetFilePath = $env:targetFilePath
     
-    # Define your GitHub personal access token
-    $githubToken = $git_token  # Replace with your GitHub token
+    # # Define your GitHub personal access token
+    # $githubToken = $git_token  # Replace with your GitHub token
     
-    # Encode the content you want to update as base64
-    $updatedContent = $encryptedJsonData
-    $updatedContentBase64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($updatedContent))
+    # # Encode the content you want to update as base64
+    # $updatedContent = $encryptedJsonData
+    # $updatedContentBase64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($updatedContent))
     
-    # Define the API URL to fetch the file content from the source branch
-    $apiUrl = "https://api.github.com/repos/"+$githubUsername+"/"+$repositoryName+"/contents/"+$targetFilePath+"?ref="+$targetBranchName
+    # # Define the API URL to fetch the file content from the source branch
+    # $apiUrl = "https://api.github.com/repos/"+$githubUsername+"/"+$repositoryName+"/contents/"+$targetFilePath+"?ref="+$targetBranchName
     
-    # Set the request headers with your personal access token
-    $headers = @{
-        Authorization = "Bearer $githubToken"
-        "Content-Type" = "application/json"
-    }
+    # # Set the request headers with your personal access token
+    # $headers = @{
+    #     Authorization = "Bearer $githubToken"
+    #     "Content-Type" = "application/json"
+    # }
     
-    # Check if the file already exists in the repository and fetch its current SHA
-    $fileExists = $false
-    $sha = $null
-    try {
-        $fileContent = Invoke-RestMethod -Uri $apiUrl -Headers @{ Authorization = "Bearer $githubToken" }
-        $fileExists = $true
-        $sha = $fileContent.sha
-    }
-    catch {
-        # The file doesn't exist
-    }
+    # # Check if the file already exists in the repository and fetch its current SHA
+    # $fileExists = $false
+    # $sha = $null
+    # try {
+    #     $fileContent = Invoke-RestMethod -Uri $apiUrl -Headers @{ Authorization = "Bearer $githubToken" }
+    #     $fileExists = $true
+    #     $sha = $fileContent.sha
+    # }
+    # catch {
+    #     # The file doesn't exist
+    # }
     
-    # Create a JSON body for the API request
-    $requestBody = @{
-        "branch" = $targetBranchName  # Corrected variable name
-        "message" = "Update Decrypted Data"
-        "content" = $updatedContentBase64  # Use the base64-encoded content
-        "sha" = $sha  # Include the current SHA
-    } | ConvertTo-Json
+    # # Create a JSON body for the API request
+    # $requestBody = @{
+    #     "branch" = $targetBranchName  # Corrected variable name
+    #     "message" = "Update Decrypted Data"
+    #     "content" = $updatedContentBase64  # Use the base64-encoded content
+    #     "sha" = $sha  # Include the current SHA
+    # } | ConvertTo-Json
     
-    # Determine whether to make a PUT or POST request based on whether the file exists
-    if ($fileExists) {
-        # File already exists, make a PUT request to update it
-        try {
-            Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method PUT -Body $requestBody
+    # # Determine whether to make a PUT or POST request based on whether the file exists
+    # if ($fileExists) {
+    #     # File already exists, make a PUT request to update it
+    #     try {
+    #         Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method PUT -Body $requestBody
     
-            Write-Host "Encrypted data has been successfully updated in $targetFilePath in branch $targetBranchName."
-        }
-        catch {
-            Write-Host "An error occurred while updating the file: $_"
-        }
-    }
-    else {
-        # File doesn't exist, make a POST request to create it
-        try {
-            Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method PUT -Body $requestBody
+    #         Write-Host "Encrypted data has been successfully updated in $targetFilePath in branch $targetBranchName."
+    #     }
+    #     catch {
+    #         Write-Host "An error occurred while updating the file: $_"
+    #     }
+    # }
+    # else {
+    #     # File doesn't exist, make a POST request to create it
+    #     try {
+    #         Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method PUT -Body $requestBody
     
-            Write-Host "Decrypted data has been successfully created in $targetFilePath in branch $targetBranchName."
-        }
-        catch {
-            Write-Host "An error occurred while creating the file: $_"
-        }
-    }
+    #         Write-Host "Decrypted data has been successfully created in $targetFilePath in branch $targetBranchName."
+    #     }
+    #     catch {
+    #         Write-Host "An error occurred while creating the file: $_"
+    #     }
+    # }
 
 }
 catch {
