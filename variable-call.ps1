03a127f153d8359c131590d292a4d7c510075b30
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-27 19:14:31 +0530

    Update variable-call.ps1

--- variable-call.ps1
+++ variable-call.ps1
@@ -1,5 +1,6 @@
-# Set the variable to send
-$variable = "Hello, world!"
+# Inside scripts.ps1, set a variable and print it
+$my_variable = "Hello, this is a variable from the script!"
+Write-Host $my_variable
 
-# Print the variable to the output stream
-echo "::set-output name=my_variable::$variable"
+# Set the variable as an output variable
+echo "::set-output name=my_output::$my_variable"
