0eb418509b0c69783e0739ce8aeb44d19d3ebc3b
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-10-02 17:32:46 +0530

    Update variable-call.ps1

--- variable-call.ps1
+++ variable-call.ps1
@@ -1,26 +1,72 @@
-# # Inside scripts.ps1, set a variable and print it
-# $my_variable = "Hello, this is a variable from the script!"
-# Write-Host $my_variable
+# Check if summary.txt file exists, if not, create it
+$filePath = "summary.txt"
+if (-not (Test-Path -Path $filePath)) {
+    New-Item -Path $filePath -ItemType File | Out-Null
+}
 
-# # Set the variable as an output variable
-# echo "::set-output name=my_output::$my_variable"
+# Sample data to add to the file
+$sampleText = "sample text to store in the summary file to upload into the artifacts...!"
 
-# Inside variable-call.ps1, create an array of messages
-$messages = @("apps is created", "developers is created", "error in kvm")
 
-# Convert the array to a JSON string (optional, depends on your use case)
-$jsonMessages = $messages | ConvertTo-Json
+Write-Host $sampleData
 
-# Print the messages to the workflow log
-Write-Host "Messages: $messages"
-Write-Host "JSON Messages: $jsonMessages"
+# Read the existing content of the file
+$fileContent = Get-Content -Path $filePath -Raw
 
-$response = Invoke-RestMethod -Uri "https://httpbin.org/get" -Method:Get -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60 -OutFile "test.json"
-Write-Host "Response: $response"
+# Append the sample data to the file content
+$newContent = $fileContent + "`n" + $sampleData
 
-# Set the messages as environment variables
-Write-Host "::set-output name=MY_MESSAGES::$messages"
-Write-Host "::set-output name=MY_JSON_MESSAGES::$jsonMessages"
+# Write the updated content back to the file
+$newContent | Set-Content -Path $filePath
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # Inside scripts.ps1, set a variable and print it
+# # $my_variable = "Hello, this is a variable from the script!"
+# # Write-Host $my_variable
+
+# # # Set the variable as an output variable
+# # echo "::set-output name=my_output::$my_variable"
+
+# # Inside variable-call.ps1, create an array of messages
+# $messages = @("apps is created", "developers is created", "error in kvm")
+
+# # Convert the array to a JSON string (optional, depends on your use case)
+# $jsonMessages = $messages | ConvertTo-Json
+
+# # Print the messages to the workflow log
+# Write-Host "Messages: $messages"
+# Write-Host "JSON Messages: $jsonMessages"
+
+# $response = Invoke-RestMethod -Uri "https://httpbin.org/get" -Method:Get -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60 -OutFile "test.json"
+# Write-Host "Response: $response"
+
+# # Set the messages as environment variables
+# Write-Host "::set-output name=MY_MESSAGES::$messages"
+# Write-Host "::set-output name=MY_JSON_MESSAGES::$jsonMessages"
 
 
 
